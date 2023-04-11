08b6f15d312c2383212f6493481c0d119ba575e0
AashikaL <aashikaaashika1799@gmail.com>
2023-04-11 10:06:21 +0530

    updated

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -8,64 +8,64 @@ import { AllProductItemsService } from '../product_service/all-product-items.ser
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.scss']
 })
-export class CartComponent implements OnInit{
-  qty:any =1;
-//  cartItems: any[]=[];
-//  cartData: any[]=[];
- totalPrice: number = 0;
-  constructor( public cs :CartItemsService, private router : Router ){
+export class CartComponent implements OnInit {
+  qty: any = 1;
+  //  cartItems: any[]=[];
+  //  cartData: any[]=[];
+  totalPrice: number = 0;
+  constructor(public cs: CartItemsService, private router: Router) {
     let datas = cs.getData();
-    if(datas){
-      datas.forEach((val :any) => {
+    if (datas) {
+      datas.forEach((val: any) => {
         this.cs.cartData.push(val)
 
       });
     }
     let clothData = cs.getclothing();
-    if(clothData){
-      clothData.forEach((val:any) => {
+    if (clothData) {
+      clothData.forEach((val: any) => {
         this.cs.cartData.push(val)
       });
     }
-    console.log(this.cs.cartData)
-}
-ngOnInit(){
-  // this.addToCart();
-}
-// addToCart(){
-//   this.cartItems = this.cs.getData();
-//   this.cartItems = this.cs.getclothing();
-// }
-changeQty(item: any, changeType: any,) {
- this.cs.cartData.find(ci => {
-    if(ci.id === item.id){
-      if(changeType === '+') {
-        ci.qty = ci.qty + 1
-        // this.qty ++;
-      } else if (changeType === '-') {
-        ci.qty = ci.qty - 1
-        // this.qty --;
+    console.log('data', this.cs.cartData)
+  }
+  ngOnInit() {
+    // this.addToCart();
+  }
+  // addToCart(){
+  //   this.cartItems = this.cs.getData();
+  //   this.cartItems = this.cs.getclothing();
+  // }
+  changeQty(item: any, changeType: any,) {
+    this.cs.cartData.find(ci => {
+      if (ci.id === item.id) {
+        if (changeType === '+') {
+          ci.qty = ci.qty + 1
+          // this.qty ++;
+        } else if (changeType === '-') {
+          ci.qty = ci.qty - 1
+          // this.qty --;
+        }
       }
-    }
-  })
-  console.log(this.cs)
-  this.cs.cartData.forEach((val: any) => {
-    this.totalPrice += val.cost * val.qty
-    // console.log('val', this.totalPrice);
-    // console.log('val', val.cost * this.qty)
-  })
-}
-clearCart() {
-  this.cs.cartData = [];
-  localStorage.removeItem('data');
-  localStorage.removeItem('clothing');
-  return this.cs.cartData
-}
-removeItem(i: number): void {
-  this.cs.cartData.splice(i, 1);
-} 
-test(qty: any) {
-  console.log(qty)
-}
+    })
+    console.log(this.cs)
+    this.cs.cartData.forEach((val: any) => {
+      this.totalPrice += val.cost * val.qty
+      // console.log('val', this.totalPrice);
+      // console.log('val', val.cost * this.qty)
+    })
+  }
+  clearCart() {
+    this.cs.cartData = [];
+    localStorage.removeItem('data');
+    localStorage.removeItem('clothing');
+    return this.cs.cartData
+  }
+  removeItem(i: number): void {
+    this.cs.cartData.splice(i, 1);
+  }
+  test(qty: any) {
+    console.log(qty)
+  }
 }
 
